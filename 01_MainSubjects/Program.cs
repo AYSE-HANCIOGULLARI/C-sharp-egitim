179a6d47ed798d72347d2905ee3b84abd9c72f04
user <user@LAPTOP-D1RGPOGP>
2024-10-04 23:17:38 +0300

    Add project files.

---
+++ 01_MainSubjects/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_MainSubjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+
+        #region YazdırmaKomutları
+
+        //Console.WriteLine("Merhaba Dünya");
+        //Console.Write("Selam");
+
+        //Console.WriteLine("*****Yemek Kategorileri*****");
+        //Console.WriteLine();
+        //Console.WriteLine("1-Çorbalar");
+        //Console.WriteLine("2-Ana Yemekler");
+        //Console.WriteLine("3-Soğuk Başlangıçlar");
+        //Console.WriteLine("4-Salatalar");
+        //Console.WriteLine("5-Tatlılar");
+        //Console.WriteLine("6-İçecekler");
+        //Console.WriteLine();
+        //Console.WriteLine("*****Yemek Kategorileri*****");
+
+
+        #region Değişkenler
+        //string
+        //Değişken_türü değişken_adı;
+
+           //string name;
+           //name="Ayşe";
+           //Console.Write(name);
+
+        string customerName;
+        string customerSurname;
+        string customerPhone;
+        string customerEmail, district, city;
+         
+        customerName = "Ali";
+        customerSurname = "Çınar";
+        customerPhone = "+0534 322 20 20";
+        customerEmail = "deneme@gmail.com";
+        District = "Kadıköy";
+        City = "İstanbul";
+
+            Console.Writeline("*****Rezarvasyon Kartı*****");
+            Console.Writeline();
+
+           
+
+
+        
+
+
+
+
+        #endregion
+            Console.Read();
+        }
+    }
+}
